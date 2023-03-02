7e0a9ff9cb855f316397ee0ba751f745628b7429
MasakOnline <91846264+mikhaelalpha@users.noreply.github.com>
2023-03-02 03:47:01 +0000

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+if(navigator.userAgent.includes("Instagram")) {
+    document.getElementById("home").style.overflow = "hidden"
+} else {
+    document.getElementById("home").style.overflow = "auto"
+}
